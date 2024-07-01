e72a7788ae953f95f5c30c1836879cad721e895e
João Pedro Monteiro <joaopedrobdmgbr@gmail.com>
2024-06-30 23:37:07 -0300

    Creating the User controller and applying a small correction to the User Model.

---
+++ BlazorAuthAPI/Api/Users/Controller/UsersController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorAuthAPI.Api.Users.Controller
+{
+    public class UsersController : ControllerBase
+    {
+    }
+}
--- BlazorAuthAPI/Core/Models/User.cs
+++ BlazorAuthAPI/Core/Models/User.cs
@@ -3,7 +3,7 @@
     public class User
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public string? Nome { get; set; }
+        public string? Name { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
         public string? Cpf { get; set; }
