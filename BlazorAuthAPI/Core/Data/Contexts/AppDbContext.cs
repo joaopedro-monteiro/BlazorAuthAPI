40e2617ded0135fcd55d40455175235d1ee38637
João Pedro Monteiro <joaopedrobdmgbr@gmail.com>
2024-06-30 23:18:51 -0300

    Creating the DbContext

---
+++ BlazorAuthAPI/Core/Data/Contexts/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BlazorAuthAPI.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorAuthAPI.Core.Data.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
