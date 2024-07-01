067ca7c41a92d55c98ff859e7e6fe22bf70fd4d0
João Pedro Monteiro <joaopedrobdmgbr@gmail.com>
2024-06-30 22:58:29 -0300

    Creating the Core folder and the first Model of the project.

---
+++ BlazorAuthAPI/Core/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorAuthAPI.Core.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Cpf { get; set; }
+        public string? Role { get; set; }
+    }
+}
